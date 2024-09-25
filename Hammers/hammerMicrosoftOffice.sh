d2c881d5617b37dec52b3636f403fb4053361a34
ScrawnyLizard <gregorymdunaway@gmail.com>
2024-09-25 15:34:09 -0600

    Office Testing
    Office can get stuck in a state of prompting for a login even after volume license. I was unable to resolve it with this script, but it does not appear on a fresh install. Collaborators can still sign into Office but won't have a 365 license. If the volume license is there it will still be activated on their login.

--- Hammers/hammerMicrosoftOffice.sh
+++ Hammers/hammerMicrosoftOffice.sh
@@ -1,6 +1,45 @@
 #!/bin/bash
 
-consoleuser=$(ls -l /dev/console | awk '{ print $3 }')
+targets=""
 
-rm /Applications/Microsoft*
-rm /Users/$consoleuser/Library/Containers/com.microsoft*
+aim() {
+  echo "Enter a key term:"
+  read aim
+  targets="$(locate $aim)"
+}
+
+confirmTargets() {
+  echo "Targets:"
+
+  i=0
+  echo "$targets" |
+  while IFS=$(echo -en "\n\b") read -r line; do
+    ((i++))
+    echo "$i: $line"
+  done
+
+  echo "Execute targets? [y|n]"
+  read confirm
+  if [[ $confirm != [Yy] ]]; then
+    echo "Disengaging..."
+    exit
+  fi
+}
+
+execute() {
+  echo "$targets" |
+  while IFS=$(echo -en "\n\b") read -r line; do
+    rm "$line"
+  done
+}
+
+survey() {
+  #Update locate db
+  /usr/libexec/locate.updatedb
+}
+
+survey
+aim
+confirmTargets
+execute
+survey
---
+++ uninstallOffice.sh
@@ -0,0 +1,53 @@
+#!/bin/zsh
+
+# uninstallOffice.sh
+# By Greg Dunaway
+# Following Microsofts recommendations here:
+# https://support.microsoft.com/en-us/office/troubleshoot-office-for-mac-issues-by-completely-uninstalling-before-you-reinstall-ec3aa66e-6a76-451f-9d35-cba2e14e94c0
+
+# Get current user
+currentUser="test"
+
+# Apps
+rm -rf /Applications/Microsoft\ Excel.app 2>/dev/null
+rm -rf /Applications/Microsoft\ OneNote.app 2>/dev/null
+rm -rf /Applications/Microsoft\ PowerPoint.app 2>/dev/null
+rm -rf /Applications/Microsoft\ Word.app 2>/dev/null
+
+# Library Files
+rm /Library/LaunchDaemons/com.microsoft.office.licensingV2.helper.plist 2>/dev/null
+rm /Library/LaunchDaemons/com.microsoft.autoupdate.helper.plist 2>/dev/null
+rm /Library/LaunchDaemons/com.microsoft.onedriveupdaterdaemon.plist 2>/dev/null
+
+rm /Library/LaunchAgents/com.microsoft.update.agent.plist 2>/dev/null
+
+rm /Library/PrivilegedHelperTools/com.microsoft.office.licensingV2.helper 2>/dev/null
+rm /Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper 2>/dev/null
+
+rm /Library/Preferences/com.microsoft.office.licensingV2.* 2>/dev/null
+
+# User Library Files
+rm /Users/$currentUser/Library/Containers/com.microsoft.errorreporting 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.Excel 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.netlib.shipassertprocess 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.Office365ServiceV2 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.onedrive.findersync 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.Outlook 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.Powerpoint 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.RMS-XPCService 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.Word 2>/dev/null
+rm /Users/$currentUser/Library/Containers/com.microsoft.onenote.mac 2>/dev/null
+
+rm /Users/$currentUser/Library/Cookies/com.microsoft.onedrive.binarycookies 2>/dev/null
+rm /Users/$currentUser/Library/Cookies/com.microsoft.onedriveupdater.binarycookies 2>/dev/null
+
+rm /Users/$currentUser/Library/Group\ Containers/UBF8T346G9.ms 2>/dev/null
+rm /Users/$currentUser/Library/Group\ Containers/UBF8T346G9.Office 2>/dev/null
+rm /Users/$currentUser/Library/Group\ Containers/UBF8T346G9.OfficeOneDriveSyncIntegration 2>/dev/null
+rm /Users/$currentUser/Library/Group\ Containers/UBF8T346G9.OfficeOsfWebHost 2>/dev/null
+rm /Users/$currentUser/Library/Group\ Containers/UBF8T346G9.OneDriveStandaloneSuite 2>/dev/null
+
+rm -rf /Users/$currentUser/Library/Saved\ Application\ State/com.microsoft.* 2>/dev/null
+
+
+echo "Microsoft Office files have been removed"
